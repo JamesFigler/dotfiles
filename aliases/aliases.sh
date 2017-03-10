06a4c5ce666a3f75077dde878d7516b3ecb6fbdb
James Figler <JamesFigler@users.noreply.github.com>
2017-03-09 22:28:56 -0500

    switched back to __git_ps1, added git hist alias

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -4,3 +4,5 @@
 
 alias ls="ls --color=auto"
 alias ll="ls -al --color=auto"
+
+git config --global alias.hist "log --pretty=format:'%C(auto)%h %Cblue%cn %Cgreen(%cr) %C(reset)%s'"
--- git-prompt/git-prompt.sh
+++ git-prompt/git-prompt.sh
@@ -5,20 +5,8 @@
 RESET="\[\033[0m\]"
 RED="\[\033[0;31m\]"
 GREEN="\[\033[1;32m\]"
-YELLOW="\[\033[1;33m\]"
 CYAN="\[\033[0;36m\]"
 PURPLE="\[\033[0;35m\]"
 
-parse_git_state() {
-  if [[ $(git status --short) ]]; then
-    echo " *"
-  fi
-}
-
-parse_git_branch() {
-  if [[ -d "$(pwd)/.git" ]]; then
-    echo "($(git symbolic-ref --short HEAD 2> /dev/null)$(parse_git_state))"
-  fi
-}
-
-export PS1="\n$GREEN\u@\h $PURPLE\w $CYAN\$(parse_git_branch)\n$RED\$$RESET "
+export GIT_PS1_SHOWDIRTYSTATE=1
+export PS1="\n$GREEN\u@\h $PURPLE\w $CYAN\$(__git_ps1 '(%s)')\n$RED\$$RESET "
