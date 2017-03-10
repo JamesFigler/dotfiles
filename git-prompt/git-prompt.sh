70335a6ebd35217c23729d82e77e075bb872a85a
James Figler <JamesFigler@users.noreply.github.com>
2017-03-09 22:05:19 -0500

    fixed issue for non-git directories

--- git-prompt/git-prompt.sh
+++ git-prompt/git-prompt.sh
@@ -9,14 +9,16 @@ YELLOW="\[\033[1;33m\]"
 CYAN="\[\033[0;36m\]"
 PURPLE="\[\033[0;35m\]"
 
-parse_git_branch() {
-  git symbolic-ref --short HEAD 2> /dev/null
-}
-
 parse_git_state() {
-  if [[ ! -z $(git status --short) ]]; then
+  if [[ $(git status --short) ]]; then
     echo " *"
   fi
 }
 
-export PS1="\n$GREEN\u@\h $PURPLE\w $CYAN(\$(parse_git_branch)$RED\$(parse_git_state)$CYAN)\n$RED\$$RESET "
+parse_git_branch() {
+  if [[ -d "$(pwd)/.git" ]]; then
+    echo "($(git symbolic-ref --short HEAD 2> /dev/null)$(parse_git_state))"
+  fi
+}
+
+export PS1="\n$GREEN\u@\h $PURPLE\w $CYAN\$(parse_git_branch)\n$RED\$$RESET "
